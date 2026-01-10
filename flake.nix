1896bfa994c4e525c154d163ff1ab5dccc1baca7
jL2718 <redials.inept_0g@icloud.com>
2026-01-09 22:25:47 -0800

    bun

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+          ];
+        };
+      }
+    );
+}
